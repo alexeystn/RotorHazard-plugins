8f1345fbd95f2fc735a636e15a12a4d07308075f
Alexey Stankevich <alexey.stn@gmail.com>
2024-05-10 22:40:15 +0300

    Initial commit

---
+++ rh_led_handler_circles/__init__.py
@@ -0,0 +1,124 @@
+'''LED visual effects'''
+
+from eventmanager import Evt
+from led_event_manager import LEDEffect, LEDEvent, Color, ColorVal, ColorPattern, effect_delay
+import time
+import datetime
+import Config
+
+
+def decreaseBrightness(color):
+    m = 255 - 3
+    mask = (m * 256 + m) * 256 + m
+    return (color & mask) >> 2
+
+
+def drawCircle(args):
+
+    colorLow = decreaseBrightness(args['color'])
+    color = args['color']
+    strip = args['strip']
+    pos = args['position'] * args['size'] * args['size']
+
+    if args['size'] == 8:
+        full = {0: (3,5), 1: (2,6), 2: (1,7), 3: (1,7), 4: (2,6), 5: (3,5)}
+        half = {0: [2,5], 1: [1,6], 2: [], 3: [], 4: [1,6], 5: [2,5]}
+        pos += 8
+        for x in range(6):
+            for y in range(full[x][0], full[x][1]):
+                strip.setPixelColor(pos+8*x+y, color)         
+            for y in half[x]:
+                strip.setPixelColor(pos+8*x+y, colorLow)
+    elif args['size'] == 4:
+        for x in range(4):
+            full = {0: (1,3), 1: (0,4), 2: (0,4), 3: (1,3)}
+            for y in range(full[x][0], full[x][1]):
+                strip.setPixelColor(pos+4*x+y, color)
+    else:
+        strip.setPixelColor(pos, color) # handle other sizes
+
+
+def showColorCircles(args):
+
+    if 'strip' in args:
+        strip = args['strip']
+    else:
+        return False
+    
+    if 'color' in args:
+        color = args['color']
+    else:
+        color = ColorVal.WHITE
+
+    size = Config.LED['LED_ROWS']
+    args['size'] = size
+    
+    maxCount = strip.numPixels() // (size*size)
+
+    if 'count' in args:
+        count = args['count']
+    else:
+        count = maxCount
+    if count > maxCount:
+        count = maxCount
+
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, ColorVal.NONE)
+         
+    for i in range(count):
+        if Config.LED["PANEL_ROTATE"]:
+            args['position'] = maxCount - 1 - i
+        else:
+            args['position'] = i
+        drawCircle(args)
+    strip.show()
+
+
+def stagingCircles(args):
+
+    triggers = args['staging_tones']
+    args['count'] = 1
+    args['effect_fn'](args)
+
+    for i in range(1, triggers):
+        args['count'] = i + 1
+        effect_delay(1000, args)
+        args['effect_fn'](args)
+
+
+def discover():
+    return [
+        
+    LEDEffect("Circles", showColorCircles, {
+        'recommended': [Evt.RACE_START, Evt.RACE_STOP]
+        }, {
+        'pattern': ColorPattern.SOLID,
+        'time': 4
+        },
+        name="showColorCircles",
+    ),
+
+    LEDEffect("Staging Circles", stagingCircles, {
+        'manual': False,
+        'include': [Evt.RACE_STAGE],
+        'exclude': [Evt.ALL],
+        'recommended': [Evt.RACE_STAGE]
+        }, {
+        'effect_fn': showColorCircles,
+        'pattern': ColorPattern.SOLID,
+        'ontime': 4,
+        'steps': 0,
+        'outSteps': 10,
+        'time': 2
+        },
+        name="stagingCircles5",
+    ),
+    
+    ]
+
+def register_handlers(args):
+    for led_effect in discover():
+        args['register_fn'](led_effect)
+
+def initialize(rhapi):
+    rhapi.events.on(Evt.LED_INITIALIZE, register_handlers)
---
+++ rh_led_handler_clock/__init__.py
@@ -0,0 +1,266 @@
+'''LED visual effects'''
+
+from eventmanager import Evt
+from led_event_manager import LEDEffect, LEDEvent, Color, ColorVal, ColorPattern, effect_delay
+import time
+import datetime
+import Config
+
+
+class Matrix:
+
+    characters = {
+        0: [ [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0] ],
+
+        1: [ [0, 0, 1, 0, 0],
+             [0, 1, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 1, 1, 1, 0] ],
+
+        2: [ [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 1, 0],
+             [0, 0, 1, 0, 0],
+             [0, 1, 0, 0, 0],
+             [1, 1, 1, 1, 1] ],
+
+        3: [ [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 1, 1, 0],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0] ],
+
+        4: [ [0, 0, 0, 0, 1],
+             [0, 0, 0, 1, 1],
+             [0, 0, 1, 0, 1],
+             [0, 1, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 1, 1, 1, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1] ],
+
+        5: [ [1, 1, 1, 1, 1],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [1, 1, 1, 1, 0],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0] ],
+
+        6: [ [0, 0, 1, 1, 0],
+             [0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [1, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0] ],
+
+        7: [ [1, 1, 1, 1, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0] ],
+
+        8: [ [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 0] ],
+
+        9: [ [0, 1, 1, 1, 0],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [1, 0, 0, 0, 1],
+             [0, 1, 1, 1, 1],
+             [0, 0, 0, 0, 1],
+             [0, 0, 0, 1, 0],
+             [0, 1, 1, 0, 0] ],
+
+      'G': [ [0, 1, 1, 1, 1, 1, 1, 1],
+             [1, 1, 1, 1, 1, 1, 1, 1],
+             [1, 1, 0, 0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 1, 1, 1, 1],
+             [1, 1, 0, 0, 1, 1, 1, 1],
+             [1, 1, 0, 0, 0, 0, 1, 1],
+             [1, 1, 1, 1, 1, 1, 1, 1],
+             [0, 1, 1, 1, 1, 1, 1, 0] ],
+
+      'O': [ [0, 1, 1, 1, 1, 1, 1, 0],
+             [1, 1, 1, 1, 1, 1, 1, 1],
+             [1, 1, 0, 0, 0, 0, 1, 1],
+             [1, 1, 0, 0, 0, 0, 1, 1],
+             [1, 1, 0, 0, 0, 0, 1, 1],
+             [1, 1, 0, 0, 0, 0, 1, 1],
+             [1, 1, 1, 1, 1, 1, 1, 1],
+             [0, 1, 1, 1, 1, 1, 1, 0] ],
+
+      '!': [ [1, 1],
+             [1, 1],
+             [1, 1],
+             [1, 1],
+             [1, 1],
+             [0, 0],
+             [1, 1],
+             [1, 1] ],        
+    }
+
+    def __init__(self, strip, color):
+        self.strip = strip
+        self.color = color
+        self.matrix = [[0] * 40 for _ in range(8)]
+
+    def clear(self):
+        for x in range(40):
+            for y in range(8):
+                self.matrix[y][x] = 0
+
+    def putCharacter(self, char, position):
+        for y in range(8):
+            for x in range(len(self.characters[char][0])):
+                self.matrix[y][position+x] = self.characters[char][y][x]
+
+    def putColon(self, position):
+        self.matrix[2][position] = 1
+        self.matrix[5][position] = 1
+
+    def display(self):
+
+        if Config.LED['PANEL_ROTATE']:
+            self.matrix = [row[::-1] for row in self.matrix[::-1]]
+
+        for x in range(40):
+            for y in range(8):
+                pos = x * 8 + y
+                if Config.LED['INVERTED_PANEL_ROWS']:
+                    if x % 2:
+                        pos = x * 8 + (8 - y - 1)
+                if self.matrix[y][x] == 1:
+                    self.strip.setPixelColor(pos, self.color)
+                else:
+                    self.strip.setPixelColor(pos, ColorVal.NONE)
+
+
+def realTimeClock(args):
+
+    if 'strip' in args:
+        strip = args['strip']
+    else:
+        return False
+ 
+    if Config.LED['LED_COUNT'] < 8*40:
+        return False
+
+    if 'CLOCK_COLOR' in Config.LED:
+        color_name = Config.LED['CLOCK_COLOR']
+        if color_name == 'red':
+            color = Color(255, 0, 0)
+        elif color_name == 'green':
+            color = Color(0, 255, 0)
+        elif color_name == 'blue':
+            color = Color(0, 0, 255)
+        elif color_name == 'yellow':
+            color = Color(255, 127, 0)
+        else:
+            color = Color(127, 127, 127)
+
+    clock = Matrix(strip, color)
+    new_time = int(time.time())
+
+    while True:
+        prev_time = new_time
+        while prev_time == new_time:
+            new_time = int(time.time())
+            effect_delay(10, args)
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        
+        clock.clear()
+
+        clock.putCharacter(hour//10, 0)
+        clock.putCharacter(hour%10, 6)
+        #clock.putColon(12)
+        clock.putCharacter(minute//10, 14)
+        clock.putCharacter(minute%10, 20)
+        #clock.putColon(26)
+        clock.putCharacter(second//10, 28)
+        clock.putCharacter(second%10, 34)
+
+        clock.display() 
+        effect_delay(10, args)
+        strip.show()
+
+
+def goText(args):
+
+    if 'strip' in args:
+        strip = args['strip']
+    else:
+        return False
+ 
+    if Config.LED['LED_COUNT'] < 8*40:
+        return False
+
+    text = Matrix(strip, args['color'])
+    text.clear()
+    text.putCharacter('G', 10)
+    text.putCharacter('O', 19)
+    text.putCharacter('!', 28)
+    text.display()
+    strip.show()
+
+
+def discover():
+    return [
+
+    LEDEffect("Real Time Clock", realTimeClock, {
+        'include': [LEDEvent.IDLE_DONE, LEDEvent.IDLE_READY, LEDEvent.IDLE_RACING]
+        },
+        {},
+        name="realTimeClock",
+    ),
+
+    LEDEffect("GO!", goText, {
+        'recommended': [Evt.RACE_START],
+        'include': [Evt.RACE_START],
+        'exclude': [Evt.ALL]
+        }, {
+        'pattern': ColorPattern.SOLID,
+        'time': 4
+        },
+        name="go8",
+    ),
+
+    ]
+
+def register_handlers(args):
+    for led_effect in discover():
+        args['register_fn'](led_effect)
+
+def initialize(rhapi):
+    rhapi.events.on(Evt.LED_INITIALIZE, register_handlers)
